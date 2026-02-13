435ff95201046b1c175f6a33e3108264f78a1861
Limpanfx <145815319+Limpanfx@users.noreply.github.com>
2026-02-13 13:05:24 +0100

    Update webhookspammer.py

--- webhookspammer.py
+++ webhookspammer.py
@@ -18,10 +18,11 @@ import time
 #prompts
 message = input("What do you want to spam?: ")
 webhookurl = Webhook(input("Enter webhook: "))
-delay = int(input("Enter a delay: "))
+delay = float(input("Enter a delay: "))
 
 #webhook spamming time
 while True:
     time.sleep(delay)
     webhookurl.send(message)
     print("Sent.")
+
